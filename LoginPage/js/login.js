6d5d654d7cd73a285c776ff0708fdbb3b6c24088
deepsthinkstech <deepthinkstech@gmail.com>
2022-10-24 18:17:10 +0530

    Login page JS updates

--- LoginPage/js/login.js
+++ LoginPage/js/login.js
@@ -1,3 +1,17 @@
-const clickHandle = () =>  window.alert("Helloooooo There!!!!!");
+let username, password;
+const clickHandle = () => {
+    username = document.getElementById('un').value;
+    password = document.getElementById('pw').value;
+    console.log("Usernamein ", username, " Passwordin ", password);
+};
 
 // Need to add a logic where initially the button is disabled. Only when user name and password fields have values, enable the button.
+class user {
+    constructor (username, password){
+        this.username = "Teddy";
+        this.password = "bear";
+    }
+}
+const newuser = new user();
+console.log("Usernameout ", username, " Passwordout ", password); 
+console.log("Usernameoutclass ", newuser.username, " Passwordoutclass ", newuser.password)
