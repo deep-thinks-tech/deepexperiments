333248b900e718892af080609f3a28f698b01440
deepsthinkstech <deepthinkstech@gmail.com>
2025-07-01 11:39:44 +0530

    Jul 1, 2025 updates

--- timedilation/timedilation.js
+++ timedilation/timedilation.js
@@ -5,17 +5,21 @@ t0 = proper time / time measured on the travelling spacecraft
 t = dilated time / time felt by a stationary observer on earth
 c = velocity of light = 300000 km/sec
 */
+
+//https://www.wolframalpha.com/input?i=time+dilation+calculator
+//https://www.omnicalculator.com/physics/time-dilation
 document.getElementById('enterbtn').addEventListener('click', timedilation);
 
 function timedilation(){
     delta_t = document.getElementById('delta_t').value; //Dilated Time: Time For The Light From Distant Object to Reach Earth (Years)
     units = document.getElementById('transtype').value; //light years for now
     t_0 = document.getElementById('t_0').value; //Years
-    v = document.getElementById('traveller_speed').value;
-    c = 1;
-    console.log('v= ',v);   
-   lorentz_constant = 1/Math.sqrt(1-((v^2)/(c^2)))
-    console.log("Lorentz Constant = ",lorentz_constant);
-    dilated_time = t_0 * lorentz_constant;
-    console.log("Dilated Time =",dilated_time);
+    traveller_speed = document.getElementById('traveller_speed').value;
+    c = 2.998 * (10**8); //meters per second
+    v = traveller_speed * c;   
+   lorentz_factor = 1/Math.sqrt(1-((v**2)/(c**2)))
+
+    dilated_time = (t_0 * lorentz_factor);
+    document.getElementById('delta_t').innerHTML = Math.round(dilated_time,2);
+    console.log("Dilated Time =",dilated_time, " Rounded value ",Math.round(dilated_time,2));
 }
